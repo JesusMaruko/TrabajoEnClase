52f5901cb5c4a470fd73c6c0dd130d4fe895281f
unknown <jesus_enrique_500@hotmail.com>
2022-09-20 10:05:43 -0600

    Clase 2

--- Clase1/index.php
+++ Clase1/index.php
@@ -5,29 +5,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Acceso al panel</title>
-    <link rel="stylesheet" href="public/css/main.css">
+      <link rel="stylesheet" href="public/css/main.css?v=1"> 
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container">
-        <form action="">
-            <fieldset>
-                <legend>
-                    Datos de acceso
-                </legend>
-                <label for="">
-                    Email:
-                </label>
-                <input type="text" name="" placeholder="write here">
-                <label for="">
-                    Password:
-                </label>
-                <input type="password" name="" placeholder="**********">
-                <button type="submit">
-                    Acceder
-                </button>
-            </fieldset>
+    <div class="container-fluid bg-secondary" style="--bs-bg-opacity: .5;"git>
+        <div class="row align-items-center d-flex justify-content-center">
+            <div class="col-6">
+                <form action="">
+                    <fieldset>
+                        <legend>
+                            Datos de acceso
+                        </legend>
+                        <label for="">
+                            Email:
+                        </label>
+                        <input type="text" name="" placeholder="write here">            
+                        <label for="">
+                            Password:
+                        </label>
+                        <input type="password" name="" placeholder="**********">
+                        <button class="btn btn-primary" type="submit">
+                            Acceder
+                        </button>
+                    </fieldset>
 
-        </form>
+                </form>
+            </div>
+        </div>
     </div>
 </body>
 </html>

9bc1f39739a290802220f0db848cfcac9b6c84e6
JesusMaruko <jesus_enrique_500@hotmail.com>
2022-09-22 11:03:15 -0500

    Actividad 6 U4

--- Clase1/index.php
+++ Clase1/index.php
@@ -9,7 +9,7 @@
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container-fluid bg-secondary" style="--bs-bg-opacity: .5;"git>
+    <div class="container-fluid bg-secondary" style="--bs-bg-opacity: .5;">
         <div class="row align-items-center d-flex justify-content-center">
             <div class="col-6">
                 <form action="">

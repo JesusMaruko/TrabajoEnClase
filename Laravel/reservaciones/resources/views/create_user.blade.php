95ad8ed978913f8331a8efcd74db20b403d7c92a
unknown <jesus_enrique_500@hotmail.com>
2022-10-18 09:58:32 -0600

    Implementación de UserController y mandandolo a 4 rutas distintas

---
+++ Laravel/reservaciones/resources/views/create_user.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create User</title>
+</head>
+<body>
+    <form method="post" action="http://127.0.0.1:8000/users/">
+        @csrf
+        <input type="text" placeholder="name" name="name">
+
+        <input type="text" placeholder="lastname" name="lastname">
+
+        <button type="submit">Guardar</button>
+    </form>
+</body>
+</html>
--- Laravel/reservaciones/routes/web.php
+++ Laravel/reservaciones/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 /*
@@ -17,6 +18,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('users/',[UserController::class,'index']);
+Route::get('users/create', [UserController::class,'create']);
+Route::get('users/{id}', [UserController::class,'show']);
+Route::post('users/', [UserController::class,'store']);
+/*
 Route::get('/saludo', function(){
     echo "hola";
 });
@@ -32,3 +38,4 @@ Route::get('suma/{num1}/{num2}/{num3?}', function($num1,$num2,$num3=0){
 Route::post('suma/', function(Request $request){
 
 });
+*/

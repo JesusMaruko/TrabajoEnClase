12588b9e73a36b42957f0764f7f6983d0c3b5dc8
JesusMaruko <jesus_enrique_500@hotmail.com>
2022-11-03 10:32:16 -0600

    Actividad 20 - Mostrando el registro de los clientes en una tabla con blade

---
+++ Laravel/reservaciones/resources/views/clients/show.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h3>
+        Clientes
+    </h3>
+    <table>
+        <thead>
+            <tr>
+                <td>
+                    ID
+                </td>
+                <td>
+                    Nombre
+                </td>
+                <td>
+                    Email
+                </td>
+                <td>
+                    Telefono
+                </td>
+            </tr>
+        </thead>
+        <tbody>
+           @foreach ($clients as $client)
+               <tr>
+                    <td>
+                        {{$client->id}}
+                    </td>
+                    <td>
+                        {{$client->name}}
+                    </td>
+                    <td>
+                        {{$client->email}}
+                    </td>
+                    <td>
+                        {{$client->phone_number}}
+                    </td>
+               </tr>
+           @endforeach
+        </tbody>
+    </table>
+</body>
+</html>

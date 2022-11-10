5192d9c3f21bb45dcfbfcfce6069d27cf958359c
JesusMaruko <jesus_enrique_500@hotmail.com>
2022-11-10 11:09:27 -0600

    Actividad 23 - Implementación de plantilla con blade layouts

---
+++ plantilla-proyecto/resources/views/layouts/app.blade.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg" data-sidebar-image="none" data-preloader="disable">
+    
+ @include('layouts.head')
+
+<body>
+
+    <!-- Begin page -->
+    <div id="layout-wrapper">
+
+        
+        @include('layouts.navbar')
+        
+        <!-- ========== App Menu ========== -->
+        
+        @include('layouts.sidebar')
+        
+        <!-- Left Sidebar End -->
+        <!-- Vertical Overlay-->
+        <div class="vertical-overlay"></div>
+
+        <!-- ============================================================== -->
+        <!-- Start right Content here -->
+        <!-- ============================================================== -->
+        <div class="main-content">
+
+            <div class="page-content">
+                <div class="container-fluid">
+
+                    <!-- start page title -->
+                    @include('layouts.bread')
+
+                    @yield('contenido')
+                    <!-- end page title -->
+
+                </div>
+                <!-- container-fluid -->
+            </div>
+            <!-- End Page-content -->
+
+            @include('layouts.footer')
+        </div>
+        <!-- end main content-->
+
+    </div>
+    <!-- END layout-wrapper -->
+
+
+
+    <!--start back-to-top-->
+    <button onclick="topFunction()" class="btn btn-danger btn-icon" id="back-to-top">
+        <i class="ri-arrow-up-line"></i>
+    </button>
+    <!--end back-to-top-->
+
+    <!--preloader-->
+    <div id="preloader">
+        <div id="status">
+            <div class="spinner-border text-primary avatar-sm" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
+    </div>
+
+    @include('layouts.scripts')
+</body>
+
+
+</html>
